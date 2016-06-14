0e67019e1e75da23199c16fb02f63e806716bc49
Subhendra Basu <subhendra.basu@gmail.com>
2016-06-14 20:54:31 +0530

    Added the data input logic

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+using namespace std;
 
 int main() {
 	FILE* fp = fopen("./input.txt", "r");
+	std::string numStr = "";
 	char nn[100];
 	while (fscanf(fp, "%s", nn) != EOF) {
-		printf("%s", nn);
+		numStr += nn;
 	}
 	fclose(fp);
+	char* num = strdup((char*) numStr.c_str());
+	cout <<"Number Read was: \t"<<num<<"\n";
 }
