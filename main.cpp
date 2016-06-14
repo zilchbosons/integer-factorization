015c2a9e2dad03b8ce57d3fb95cf003563b5931e
Subhendra Basu <subhendra.basu@gmail.com>
2016-06-14 21:07:36 +0530

    Added Rotation Logic, Now going for the Identification of Riemann Zeros along with their Symmetries

--- main.cpp
+++ main.cpp
@@ -3,7 +3,43 @@
 #include <string.h>
 #include <stdlib.h>
 #include <string>
+#include <math.h>
+
 using namespace std;
+
+
+char* reverse_string(char* t) {
+	char* s = strdup(t);
+	for (int i = 0; i < floor(strlen(s) / 2.0); ++i) {
+		char tmp = s[i];
+		s[i] = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = tmp;
+	}
+	return s;
+}
+
+char* rotateLeft(char* n) {
+   int l = strlen(n) ;
+   int t = n[l - 1];
+   n[l-1] = n[0];
+   for (int i = 0; i < l - 2; ++i) {
+	   n[i] = n[i + 1];
+   }
+   n[l-2] = t;
+   return n;
+}
+
+char* factorize(char* num) {
+        char* nrev = reverse_string(num);
+	int l = strlen(num);
+	for (int i = 0; i < ceil(l / 2); ++i) {
+            cout << "Number stack is :\n";
+            cout << num <<"\n";
+	    cout << nrev <<"\n";
+	    rotateLeft(nrev);
+	}
+	return 0;
+}
 
 int main() {
 	FILE* fp = fopen("./input.txt", "r");
@@ -15,4 +51,5 @@ int main() {
 	fclose(fp);
 	char* num = strdup((char*) numStr.c_str());
 	cout <<"Number Read was: \t"<<num<<"\n";
+        char* factor = factorize(num);
 }
